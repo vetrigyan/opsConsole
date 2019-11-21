ae057f44627eabb4463b8fc59b7f254858a57220
EC2 Default User <ec2-user@ip-172-31-22-181.ec2.internal>
2019-11-21 23:21:53 +0000

    UI view changes

--- src/components/Sensors.js
+++ src/components/Sensors.js
@@ -38,10 +38,15 @@ export default () => {
           setListings(prevValue => {
           console.log("Invoked onUpdateSensorRoomData Subscription callback " + e.value.data.onUpdateSensorRoomData.id);  
                 const updatedListings = prevValue.map(l => {
-                  if (l.id === e.value.data.onUpdateSensorRoomData.id) {
+                  console.log("In onUpdateSensorRoomData processing, received sensorId = " + e.value.data.onUpdateSensorRoomData.sensorId);
+                  console.log("In onUpdateSensorRoomData processing, received id = " + e.value.data.onUpdateSensorRoomData.id);
+                  if (l.sensorId == e.value.data.onUpdateSensorRoomData.sensorId) {
+                  console.log("In onUpdateSensorRoomData processing, matched");
                     return e.value.data.onUpdateSensorRoomData;
-                  } else 
+                  } else {
+                    console.log("In onUpdateSensorRoomData processing, Not matched");
                     return l;
+                  }
                 });
                 return updatedListings;
           });
