bb49d9694565e5daea541b16640f540555259131
EC2 Default User <ec2-user@ip-172-31-22-181.ec2.internal>
2019-11-22 20:37:02 +0000

    subscription for sensorstatus now using record id instead of sensorid

--- src/components/Sensors.js
+++ src/components/Sensors.js
@@ -40,7 +40,7 @@ export default () => {
                 const updatedListings = prevValue.map(l => {
                   console.log("In onUpdateSensorRoomData processing, received sensorId = " + e.value.data.onUpdateSensorRoomData.sensorId);
                   console.log("In onUpdateSensorRoomData processing, received id = " + e.value.data.onUpdateSensorRoomData.id);
-                  if (l.sensorId == e.value.data.onUpdateSensorRoomData.sensorId) {
+                  if (l.id == e.value.data.onUpdateSensorRoomData.id) {
                   console.log("In onUpdateSensorRoomData processing, matched");
                     return e.value.data.onUpdateSensorRoomData;
                   } else {
